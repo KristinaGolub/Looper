8e69bba38edc5118426da67d77df50d68dc5ced9
Голуб Кристина <kristina_golub_2014@mail.ru>
2021-06-08 00:16:06 +0300

    Начало проекта Looper

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// let nav = document.querySelector('.js-nav');
+// let sectionPreview = document.querySelector('.preview');
+
+// window.addEventListener('scroll', (e) => {
+//     e.preventDefault();
+//     if (pageYOffset > 500) {
+//         nav.classList.add('nav');
+//     }else {
+//         nav.classList.remove('nav');
+//     }
+    
+// })
